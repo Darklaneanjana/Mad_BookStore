71213d92a71dc8a70c282294bd939cf74b782db8
Darklaneanjana <darklaneanjana@gmail.com>
2022-04-30 18:12:41 +0530

    some formating before merge

--- app/src/main/java/com/example/bookstore/LoginActivity.java
+++ app/src/main/java/com/example/bookstore/LoginActivity.java
@@ -4,14 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.util.Log;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -43,21 +38,12 @@ public class LoginActivity extends AppCompatActivity {
 
 
         Button toRegister = findViewById(R.id.toRegister);
-        toRegister.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+        toRegister.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
 
-            }
-        });
-
-        login.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String txtEmail = email.getText().toString();
-                String txtPassword = password.getText().toString();
-                signIn(txtEmail,txtPassword);
-            }
+        login.setOnClickListener(v -> {
+            String txtEmail = email.getText().toString();
+            String txtPassword = password.getText().toString();
+            signIn(txtEmail,txtPassword);
         });
 
     }
@@ -77,27 +63,24 @@ public class LoginActivity extends AppCompatActivity {
     private void signIn(String email, String password) {
         // [START sign_in_with_email]
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "login una.",
-                                    Toast.LENGTH_LONG).show();
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(LoginActivity.this, "login una.",
+                                Toast.LENGTH_LONG).show();
 
-                            updateUI(user);
-                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-                            finish();
+                        updateUI(user);
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        finish();
 
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                        updateUI(null);
                     }
                 });
         // [END sign_in_with_email]
