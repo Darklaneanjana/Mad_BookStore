284204dd8634e0790ae302b7c625c234f8124f99
Darklaneanjana <darklaneanjana@gmail.com>
2022-04-30 17:45:40 +0530

    Fixed Book View

--- app/src/main/java/com/example/bookstore/BookActivity.java
+++ app/src/main/java/com/example/bookstore/BookActivity.java
@@ -1,7 +1,5 @@
 package com.example.bookstore;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,11 +7,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.bumptech.glide.Glide;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -34,14 +36,18 @@ public class BookActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags( WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_book);
 
 //
         Intent intent = getIntent();
         String message = intent.getStringExtra(HomeActivity.bookId);
-        TextView textView = findViewById(R.id.tempText);
+        TextView bookTitle = findViewById(R.id.bookTitle);
+        TextView bookAuthor = findViewById(R.id.bookAuthor);
+        TextView bookPrice = findViewById(R.id.bookPrice);
+        TextView bookRating = findViewById(R.id.bookRating);
+        TextView bookDescription = findViewById(R.id.bookDescription);
 
         DocumentReference docRef = FirebaseFirestore.getInstance().collection("Books").document("Lfuk2Z5AJxUExtIQlUoL");
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
@@ -51,7 +57,14 @@ public class BookActivity extends AppCompatActivity {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData().toString());
-                        textView.setText(document.getData().get("Author").toString());
+
+                        bookTitle.setText(document.getData().get("Title").toString());
+                        bookAuthor.setText(document.getData().get("Author").toString());
+                        bookPrice.setText(document.getData().get("Price").toString() + "0$");
+                        bookRating.setText(document.getData().get("Ratings").toString());
+                        bookDescription.setText(document.getData().get("Description").toString());
+
+
                         setCover(document.getData().get("Image").toString());
                     } else {
                         Log.d(TAG, "No such document");
@@ -62,6 +75,20 @@ public class BookActivity extends AppCompatActivity {
             }
         });
 
+        Button addToCartBtn = findViewById(R.id.addToCartBtn);
+        addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        Button RentBtn = findViewById(R.id.RentBtn);
+        RentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         ImageButton back = findViewById(R.id.backToBookList);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,18 +96,19 @@ public class BookActivity extends AppCompatActivity {
                 finish();
             }
         });
+
     }
 
-    public void setCover(String image){
+    public void setCover(String image) {
         // Reference to an image file in Cloud Storage
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Books/"+image);;
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Books/" + image);
         // ImageView in your Activity
         ImageView imageView = findViewById(R.id.bookCover);
         // [END storage_load_with_glide]
-        storageReference.getBytes(1024*1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+        storageReference.getBytes(1024 * 1024).addOnSuccessListener(new OnSuccessListener<byte[]>() {
             @Override
             public void onSuccess(byte[] bytes) {
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0,bytes.length);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                 imageView.setImageBitmap(bitmap);
                 // Data for "images/island.jpg" is returns, use this as needed
             }
