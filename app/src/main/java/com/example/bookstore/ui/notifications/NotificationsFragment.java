8156a55dade2d1eda48f904732290c9e60aade14
Tharushi Perera <82897351+hvneypop@users.noreply.github.com>
2022-05-06 20:19:58 +0530

    second

--- app/src/main/java/com/example/bookstore/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/bookstore/ui/notifications/NotificationsFragment.java
@@ -1,15 +1,19 @@
 package com.example.bookstore.ui.notifications;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.bookstore.BookActivity;
+import com.example.bookstore.FeedbackActivity;
 import com.example.bookstore.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
@@ -23,6 +27,14 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        Button textView1 = binding.toFeedbackBtn;
+        textView1.setOnClickListener(v -> {
+           // String bookId = "Lfuk2Z5AJxUExtIQlUoL";
+            Intent intent = new Intent(getContext(), FeedbackActivity.class);
+            //intent.putExtra("bookId", bookId);
+            startActivity(intent);
+        });
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
